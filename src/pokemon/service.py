1895019aad0b17208899a1d7a81c3c9eb7ee9de1
alimikegami <alimikegami1@gmail.com>
2025-01-27 10:03:40 +0800

    feat: remove pokemon insertion limit

--- src/pokemon/service.py
+++ src/pokemon/service.py
@@ -12,20 +12,12 @@ def scrape_pokemon_data(db):
     
     # handle the same pokemon from being inserted into the database multiple times, as the site also contains mega variant with the same pokedex ID   
     hash_map = {}
-
-    
-    # define the limit of pokemon we are trying to import to the database
-    count = 0
-    limit = 15
     
     if response.status_code == 200:
         html_content = response.content
         soup = BeautifulSoup(html_content, "html.parser")
         pokedex = soup.find(id="pokedex").find("tbody")
         for row in pokedex.find_all("tr"):
-            if count == limit:
-                break
-            
             name = row.find("a", class_="ent-name")
             types = row.find_all("a", class_="type-icon")
             pokedex_id = row.find(class_="infocard-cell-data")
@@ -52,7 +44,6 @@ def scrape_pokemon_data(db):
                 pokemons.append(pokemon)
             
             hash_map[pokedex_id] = 1
-            count += 1
     else:
         raise Exception("unable to access pokemondb.net")
     
